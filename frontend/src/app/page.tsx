f19da663d83cdbf14fd1758f89a06f570d1f501c
Arsenii Hrokh <a.m.groh07@gmail.com>
2026-09-18 21:57:33 +0300

    verified connection between backend and frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client"
 
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+  
+  useEffect(() => {
+    async function testApi() {
+      const res = await fetch('http://127.0.0.1:8000/test', { cache: 'no-store' });
+      const data = await res.json();
+      setMessage(data.message);
+    }
+    testApi();
+  }, [])
+
   return (
-    <div>Hello Word</div>
+    <h1>{message}</h1>
   );
 }
