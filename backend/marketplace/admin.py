d254ecccfde8e73e1ab8a67db708314282dccc93
Arsenii Hrokh <a.m.groh07@gmail.com>
2026-09-19 21:29:05 +0300

    register models and create super user

--- backend/marketplace/admin.py
+++ backend/marketplace/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Review, Property, PropertyImage, User, Booking, Amenity
 
-# Register your models here.
+
+admin.site.register(Review)
+admin.site.register(Property)
+admin.site.register(User)
+admin.site.register(PropertyImage)
+admin.site.register(Booking)
+admin.site.register(Amenity)
