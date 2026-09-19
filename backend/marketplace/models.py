adb7f4ff7cd73c945c7d2fc62ae14089010a9dd6
Arsenii Hrokh <a.m.groh07@gmail.com>
2026-09-19 21:14:58 +0300

    create booking model

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -23,3 +23,27 @@ class Property(models.Model):
 
     def __str__(self):
         return self.title
+
+class Booking(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'PE', ('Pending')
+        CONFIRMED = 'CO', ('Confirmed')
+        CANCELLED = 'CA', ('Cancelled')
+        COMPLETED = 'COM', ('Completed')
+
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='bookings')
+    check_in = models.DateField()
+    check_out = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=3,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    def clean(self):
+        if self.check_in and self.check_out and self.check_in >= self.check_out:
+            from django.core.exceptions import ValidationError
+            raise ValidationError("Check-out must be after check-in")
