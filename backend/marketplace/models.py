4655441fd7ee70bd9c111f733e2d4c9d33885852
Arsenii Hrokh <a.m.groh07@gmail.com>
2026-09-19 20:50:41 +0300

    add property model

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -8,6 +8,18 @@ def user_avatars_directory_path(instance, filename):
 class User(AbstractUser):    
     is_host = models.BooleanField(default=False)
     avatar = models.ImageField(upload_to=user_avatars_directory_path, null=True, blank=True)
-    
+
     def __str__(self):
         return self.username
+
+class Property(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='properties')
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    location = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    max_guests = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
